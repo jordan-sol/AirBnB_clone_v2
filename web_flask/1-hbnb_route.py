3e42bda60f28015eaa26c80d70856d8684760d0c
jordan-sol <101702526+jordan-sol@users.noreply.github.com>
2022-10-10 11:05:09 +0300

    Create 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" listening port 5000 """
+from flask import Flask, escape, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_framework():
+    """ display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def disp_hbnb():
+    """ display /hbnb! """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.url_map.strict_slashes = False
