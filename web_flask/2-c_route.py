8cf49eebcab2615dbe647b55f179f2cf4312c334
jordan-sol <101702526+jordan-sol@users.noreply.github.com>
2022-10-10 11:05:40 +0300

    Create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""init Flask web app"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_framework():
+    """ display Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def disp_hbnb():
+    """ display /hbnb! """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_text(text):
+    """ display /c/<text> """
+    return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run()
