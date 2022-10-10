f20c261082da726e7762fa1250e851b87f53dcaf
jordan-sol <101702526+jordan-sol@users.noreply.github.com>
2022-10-10 11:09:05 +0300

    Create 6-number_odd_or_even.py

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""init Flask web app"""
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_framework():
+    """ display Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def disp_hbnb():
+    """ display /hbnb! """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_text(text):
+    """ display /c/<text> """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route("/python")
+@app.route('/python/<text>/')
+def py_text(text='is cool'):
+    """hbnb"""
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def only_integer(n):
+    """only integers"""
+    return '%d is a number' % n
+
+
+@app.route('/number_template/<int:n>')
+def n_template(n):
+    """Num of template"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_r_even(n):
+    """Odd r even"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run()
